9190b4faefafc856136523f0f40ad4c069665d6d
Anuj Tiwari <anujtiwari9111@gmail.com>
2026-03-14 00:25:30 +0530

    Time: 0 ms (100%), Space: 8.3 MB (53.94%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        int i = 0, n = s.size();
+        while (i < n) {
+            if (s[i] >= 65 && s[i] <= 90) {
+                s[i] += 32;
+            }
+            i++;
+        }
+        return s;
+    };
+};
